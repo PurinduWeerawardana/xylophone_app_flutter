1eef4f865265245c98affe6447f23e284361a6c2
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2022-07-26 19:27:34 +0530

    Initial commit with UI

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Expanded drawKeys({required Color color, required int soundNumber}) {
+    return (Expanded(
+      child: TextButton(
+        onPressed: () {},
+        child: Container(
+          color: color,
+        ),
+      ),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              drawKeys(color: Colors.red, soundNumber: 1),
+              drawKeys(color: Colors.orange, soundNumber: 2),
+              drawKeys(color: Colors.yellow, soundNumber: 3),
+              drawKeys(color: Colors.green, soundNumber: 4),
+              drawKeys(color: Colors.blue, soundNumber: 5),
+              drawKeys(color: Colors.indigo, soundNumber: 6),
+              drawKeys(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
